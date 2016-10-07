01b7dcc658a3a1f9dc23ff98cc8550634701d875
Andrushchenko, Oleksandr <andr2000@gmail.com>
2016-10-07 10:23:35 +0300

    Fix: module fuse is required for sshfs to work
    Signed-off-by: Andrushchenko, Oleksandr <andr2000@gmail.com>

--- mount-sshfs.sh
+++ mount-sshfs.sh
@@ -3,5 +3,6 @@ MOUNT_POINT=~/hostfs
 echo "Reading config.cfg" >&2
 mkdir -p ${MOUNT_POINT}
 source config.cfg
+modprobe fuse
 echo "Mounting host folder ${user}@${host}:${path} to ${MOUNT_POINT}"
 ./sshfs -o allow_other ${user}@${host}:${path} ${MOUNT_POINT}
